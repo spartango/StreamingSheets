7f1158f7b8b031a496f3a00596bc0adbd403b788
Anand Gupta <spartango@gmail.com>
2014-06-23 22:36:18 -0700

    minor changes

--- src/main/java/com/irislabs/demo/TCGAMerge.java
+++ src/main/java/com/irislabs/demo/TCGAMerge.java
@@ -1,12 +1,18 @@
 package com.irislabs.demo;
 
 import com.irislabs.sheet.FileSheet;
+import com.irislabs.sheet.SheetEntry;
 import com.irislabs.sheet.SheetMerger;
 import com.irislabs.sheet.SheetWriter;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 /**
  * Author: spartango
@@ -14,27 +20,79 @@ import java.util.List;
  * Time: 11:03.
  */
 public class TCGAMerge {
+    private static final Path   PATH   = FileSystems.getDefault()
+                                                    .getPath("/Users/spartango/Dropbox/Iris/TCGA Clinical/");
+    private static final String OUTPUT = "/Users/spartango/Dropbox/Iris/TCGA Clinical/";
+
     public static void main(String[] args) throws IOException {
-        FileSheet patient = new FileSheet("prad/patient.txt");
-        FileSheet radiation = new FileSheet("prad/radiation.txt");
-        FileSheet drug = new FileSheet("prad/drug.txt");
-        FileSheet omf = new FileSheet("prad/omf.txt");
-        FileSheet cqcf = new FileSheet("prad/cqcf.txt");
-        FileSheet nte = new FileSheet("prad/nte.txt");
-        FileSheet followup_15 = new FileSheet("prad/followup_10.txt");
+        final Map<String, List<Path>> filesByCancer = Files.list(PATH)
+                                                           .filter(path -> path.toString().contains("nationwide"))
+                                                           .collect(
+                                                                   Collectors.groupingBy(path -> {
+                                                                       String[] parts = path.toString().split("_");
+                                                                       return parts[parts.length - 1].split("\\.")[0];
+                                                                   }));
 
-        List<FileSheet> sheets = Arrays.asList(patient,
-                                               radiation,
-                                               drug,
-                                               cqcf,
-                                               omf,
-                                               nte,
-                                               followup_15);
+        filesByCancer.forEach((cancer, paths) -> {
+            SheetMerger merger = new SheetMerger();
+            paths.stream()
+                 .map(Path::toString)
+                 .map(path -> {
+                     try {
+                         return new FileSheet(path);
+                     } catch (Exception e) {
+                         System.out.println("Error loading " + cancer + " " + path + ": " + e);
+                         return null;
+                     }
+                 }).filter(sheet -> sheet != null)
+                 .forEach(sheet -> {
+                     try {
+                         merger.append(sheet);
+                     } catch (Exception e) {
+                         System.out.println("Error appending " + cancer + ": " + e);
+                     }
+                 });
+            
+            final Collection<SheetEntry> merged = merger.getMerged();
 
-        SheetMerger merger = new SheetMerger();
-        sheets.forEach(merger::append);
+            try {
+                SheetWriter writer = new SheetWriter(OUTPUT + cancer + "_merged.txt", merger.getFields());
+                merged.forEach(writer);
+            } catch (Exception e) {
+                System.out.println("Error writing " + cancer + ": " + e);
+            }
 
-        SheetWriter writer = new SheetWriter("prad/merged.txt", merger.getFields());
-        merger.getMerged().forEach(writer);
+            try {
+                SheetWriter numeric = new SheetWriter(OUTPUT + cancer + "_numeric.txt", merger.getFields());
+                Map<String, AtomicInteger> counterMap = new HashMap<>();
+                Map<String, Integer> numericMap = new TreeMap<>();
+                merger.getFields().forEach(field -> counterMap.put(field, new AtomicInteger(0)));
+
+                merged.stream().map(patient -> {
+                    SheetEntry newEntry = new SheetEntry();
+                    patient.forEach((key, value) -> {
+                        if (StringUtils.isNumeric(value)) {
+                            // Try to parse the value as is
+                            newEntry.put(key, Double.parseDouble(value));
+                        } else {
+                            // If this value hasn't been seen before
+                            String kvPair = key + "_" + value;
+
+                            if (!numericMap.containsKey(kvPair)) {
+                                int newValue = counterMap.get(key).getAndIncrement();
+                                numericMap.put(kvPair, newValue);
+                            }
+
+                            newEntry.put(key, numericMap.get(kvPair));
+                        }
+                    });
+                    return newEntry;
+                }).forEach(numeric);
+
+            } catch (Exception e) {
+                System.out.println("Error numeric " + cancer + ": " + e);
+            }
+        });
+
     }
 }
